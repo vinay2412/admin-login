30ffdb58701cd98a1d3c99018f863be38800c2bb
Vinod Prajapati <vinodprajapatimj@gmail.com>
2022-07-15 10:22:12 +0530

    working progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/login' }
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
